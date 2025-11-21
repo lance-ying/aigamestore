77cde3a6dbb2253fdc55dd62641f31ac3ce81aed
Ryan Truong <143540876+heyodog0@users.noreply.github.com>
2025-11-21 12:24:42 -0500

    update more

--- games/single_prompt_with_testing/game_0004/sample_0/mini_games/all_games.js
+++
@@ -1,7 +0,0 @@
-export { HoopFever } from './hoop_fever.js';
-export { FormulaRace } from './formula_race.js';
-export { PenaltyShot } from './penalty_shot.js';
-export { HomeRunDerby } from './home_run_derby.js';
-export { SpeedSkate } from './speed_skate.js';
-export { SurfMaster } from './surf_master.js';
-export { TennisAce } from './tennis_ace.js';
